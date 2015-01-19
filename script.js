e89397b9e85194ccff1032a418310219e32a9c73
Nick Stares <Nick Stares>
2015-01-18 16:30:31 -0800

    message

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-
 window.addEventListener("load", function () {
+
+
+				  var boxOne = document.getElementById("a1");
+				  var boxTwo = document.getElementById("a2");
+				  var boxThree = document.getElementById("a3");
+				  var boxFour = document.getElementById("a4");
+				  var boxFive = document.getElementById("a5");
+				  var boxSix = document.getElementById("a6");
+				  var boxSeven = document.getElementById("a7");
+				  var boxEight = document.getElementById("a8");
+				  var boxNine = document.getElementById("a9");
+
+
+
+				  var boardMap = [
+				  		   [boxOne.innerHTML,
+				  		   boxTwo.innerHTML,
+				  		   boxThree.innerHTML],
+
+				  		   [boxFour.innerHTML,
+				  		   boxFive.innerHTML,
+				  		   boxSix.innerHTML],
+
+				  		   [boxSeven.innerHTML,
+				  		   boxEight.innerHTML,
+				  		   boxNine.innerHTML]
+
+				  ];
 
 
 
@@ -10,50 +37,56 @@ window.addEventListener("load", function () {
 		}
 
 		var clicks = 0;
+		
+
+	
+			
+		
 
 		function clickFunction(event) {
+
 			clicks += 1;
 			if (clicks % 2 === 0){
 				event.target.innerHTML = "O";
+
+				}
 				
 
-			}
 				else {
-				event.target.innerHTML = "X"
+				event.target.innerHTML = "X";
 
-				;
-				
 
 				}
 			
 				event.target.style.backgroundColor = "yellow";
+						
+					if (boardMap[0][0] == "X" && boardMap[0][1] == "X" && boardMap[0][2] == "X" ) {
+						console.log("h");
+					}
+				
 				event.target.removeEventListener("click", clickFunction );
+				
 
-
+				
 		}
 
+		console.log(boardMap);
 
-
-
-
-			
-
+		
 
 
 		function resetFunction (event) {
 		for (var i = 0; i < boxes.length; i++) {
 		 boxes[i].innerHTML = " ";
 		 boxes[i].style.backgroundColor = "white";
+		 boxes[i].addEventListener("click", clickFunction);
 
-		}
+			}
 		
 		}
 
 		var reset1 = document.getElementsByClassName("reset");
 		reset1[0].addEventListener("click", resetFunction);
-
-
-
 
 
 });
